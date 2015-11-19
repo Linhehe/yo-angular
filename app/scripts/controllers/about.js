4e95cc59dfccbb7b1292490fd2027bd07d88c6b0
Linhehe <1215634430@qq.com>
2015-11-19 10:59:26 +0800

    yo-angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name personalBlogApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the personalBlogApp
+ */
+angular.module('personalBlogApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name personalBlogApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the personalBlogApp
+ */
+angular.module('personalBlogApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
